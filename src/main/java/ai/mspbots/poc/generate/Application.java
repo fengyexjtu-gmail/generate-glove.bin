bf7c43f6252c6dfc5e70028d1e2a79f28e49ad4d
Jeffery.Gao <Jeffery.Gao@mspbots.ai>
2024-10-11 12:12:23 +0800

    path

--- src/main/java/ai/mspbots/poc/generate/Application.java
+++ src/main/java/ai/mspbots/poc/generate/Application.java
@@ -7,7 +7,7 @@ import java.util.ArrayList;
 
 public class Application {
     public static void main(String[] args) {
-        Word2Vec wordVectors = WordVectorSerializer.readWord2VecModel(new File("/data/poc/build/glove.6B.300d.txt"));
-        WordVectorSerializer.writeWord2VecModel(wordVectors, new File("/data/poc/build/glove.bin"));
+        Word2Vec wordVectors = WordVectorSerializer.readWord2VecModel(new File("/opt/glove.6B.300d.txt"));
+        WordVectorSerializer.writeWord2VecModel(wordVectors, new File("/opt/glove.bin"));
     }
 }
